c5c26c0fe9d20b59afff10b39c15940527641a07
Kombiah <kombiahinhc@gmail.com>
2025-11-15 19:43:38 +0530

    New Build Folder

--- build/block-frontend.asset.php
+++ build/block-frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5122fd1d0fcae7782e1c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e6dbb640b98b5b2321e5');
